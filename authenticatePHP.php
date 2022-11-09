3a2956571aee88d24283a38ccff2400e15f847e4
Renato Alves Soares <renatoguara2020@gmail.com>
2022-11-09 15:46:58 -0300

    ok

--- authenticatePHP.php
+++ authenticatePHP.php
@@ -2,11 +2,16 @@
     if (!isset($_SERVER['PHP_AUTH_USER'])) {
         header("WWW-Authenticate: Basic realm=\"Private Area\"");
         header("HTTP/1.0 401 Unauthorized");
-        // only reached if authentication fails
-        print "Sorry - you need valid credentials granted access to the private area!\n";
+        print "Sorry - you need valid credentials to be granted access!\n";
         exit;
     } else {
-        // only reached if authentication succeeds
-        print "Welcome to the private area, {$_SERVER['PHP_AUTH_USER']} - you used {$_SERVER['PHP_AUTH_PW']} as your password.";
+        if (($_SERVER['PHP_AUTH_USER'] == 'paul') && ($_SERVER['PHP_AUTH_PW'] == 'hudson')) {
+            print "Welcome to the private area!";
+        } else {
+            header("WWW-Authenticate: Basic realm=\"Private Area\"");
+            header("HTTP/1.0 401 Unauthorized");
+            print "Sorry - you need valid credentials to be granted access!\n";
+            exit;
+        }
     }
 ?>
